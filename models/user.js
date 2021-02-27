4485f57b226cd817f1eefc327d27cba8b2f6bf35
jhaeti <jhaeti@gmail.com>
2021-02-27 12:40:17 +0000

    Created user model and route for registering a user

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
