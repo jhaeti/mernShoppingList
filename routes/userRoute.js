8c9947e0483a791c9c1db0338f6fe0b499076980
TI JHAE <jhaeti@gmail.com>
2021-03-01 17:38:29 +0000

    Login api created

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -43,5 +43,35 @@ route.post("/register", (req, res) => {
     });
   }
 });
+route.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ msg: "Please enter all fields" });
+  } else {
+    //   Check whether user already exist
+    User.findOne({ email }).then((user) => {
+      if (!user) {
+        res.status(400).json({ msg: "User doesnot exist" });
+      } else {
+        bcrypt.compare(password, user.password).then((isMatch) => {
+          if (isMatch) {
+            jwt.sign(
+              { id: user.id },
+              process.env.JWT_SECRET_KEY,
+              (err, token) => {
+                const { name, email, id } = user;
+                if (err) throw err;
+                res.json({ token, user: { name, email, id } });
+              }
+            );
+          } else {
+            res.json({ msg: "Invalid credentials" });
+          }
+        });
+      }
+    });
+  }
+});
 
 module.exports = route;
